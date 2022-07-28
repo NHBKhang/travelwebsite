2f775acbfcd0471bb6ea9777c55d7b132a5b42ec
NHBKhang <109735934+NHBKhang@users.noreply.github.com>
2022-07-28 19:38:14 +0700

    no
    none

---
+++ travelwebsite.js
@@ -0,0 +1,5 @@
+function show(){
+    document.getElementsByClassName("background").style.marginLeft="0";
+    document.getElementsByClassName("closeMenu").style.marginLeft="0";
+    document.getElementsByClassName("options").style.marginLeft="0";    
+}
