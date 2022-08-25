abbc10c31bc470791663295279e2ae883900bac1
NHBKhang <109735934+NHBKhang@users.noreply.github.com>
2022-08-25 19:52:37 +0700

    updated
    updated

--- travelwebsite.js
+++ travelwebsite.js
@@ -1,5 +1,33 @@
-function show(){
-    document.getElementsByClassName("background").style.marginLeft="0";
-    document.getElementsByClassName("closeMenu").style.marginLeft="0";
-    document.getElementsByClassName("options").style.marginLeft="0";    
-}
+$(document).ready(function(){
+    $('.slider').slick({
+      slidesToShow:4,
+      Infinity:true,
+      autoplay:true,
+      autoplaySpeed:3000,
+      slidesToScroll:1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows:false,
+          }
+        },
+        {
+          breakpoint: 860,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 590,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+    });
+  });
